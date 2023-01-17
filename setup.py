0064c9da32b8733e2c902229b80162e5eed92a26
Viktor Pavlov <viktor.pavlov.1998@list.ru>
2023-01-17 18:18:37 +0300

    #1 init
    upload scripts and descriptions

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name='gripperEnv',
+    version='0.0.1',
+    install_requires=[
+        'stable-baselines==2.10.1',
+        'tensorflow_gpu==1.14.0',
+        # если вы вдруг решили тренировать на процессоре бог вам судья
+        #'tensorflow==1.14.0'
+        'gym==0.19.0',
+        'keras==2.2.4',
+        'matplotlib==3.3.4',
+        'numpy==1.18',
+        'opencv-contrib-python==4.5.5.64',
+        'pandas==1.1.5',
+        'pybullet==3.2.5',
+        'pytest==7.0.1',
+        'pydot==1.4.2',
+        'PyYAML==5.4.1',
+        'seaborn==0.11.2',
+        'scikit-learn==0.24.2',
+        'tqdm==4.64.0',
+        'paramiko==2.10.3',
+    ],
+)
